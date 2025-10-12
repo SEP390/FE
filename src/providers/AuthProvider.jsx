5ed2693a403a24073a76de7c4494361e6e657b69
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-10-12 17:18:25 +0700

    fix: handle token change

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,10 @@
 import {AuthContext} from "../hooks/useToken.js";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export function AuthProvider({ children }) {
     const [token, setToken] = useState(localStorage.getItem("token"));
+    useEffect(() => {
+        localStorage.setItem("token", token);
+    }, [token]);
     return <AuthContext.Provider value={{token, setToken}}>{children}</AuthContext.Provider>;
 }
