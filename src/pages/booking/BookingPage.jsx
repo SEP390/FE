7ab5f7af40751f8d60a7e83d2d7110190eb3b8f9
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-10-16 20:48:49 +0700

    fix: double render error

--- src/pages/booking/BookingPage.jsx
+++ src/pages/booking/BookingPage.jsx
@@ -1,31 +1,21 @@
 import {AppLayout} from "../../components/layout/AppLayout.jsx";
 import {useEffect, useState} from "react";
 import {useApi} from "../../hooks/useApi.js";
-import {useNavigate} from "react-router-dom";
 import {Spin} from "antd";
 import {CurrentBooking} from "./CurrentBooking.jsx";
 import {BookingY1} from "./BookingY1.jsx";
 
 export function BookingPage() {
-    const {get, data, isError, isComplete} = useApi();
-    const [outlet, setOutlet] = useState(null);
+    const {get, data, isComplete, isSuccess} = useApi();
 
     useEffect(() => {
         get("/booking/current")
     }, [get]);
 
-    useEffect(() => {
-        if (!isComplete) return;
-        if (data) {
-            setOutlet(<CurrentBooking data={data}/>);
-        } else {
-            setOutlet(<BookingY1 />);
-        }
-    }, [data, isComplete]);
-
     return <Spin spinning={!isComplete}>
         <AppLayout activeSidebar={"booking"}>
-            {outlet}
+            {isSuccess && data && <CurrentBooking data={data}/>}
+            {isSuccess && !data && <BookingY1 />}
         </AppLayout>
     </Spin>
 }
