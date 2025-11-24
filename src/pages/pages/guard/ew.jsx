25ec9fffa52f59ec4f631ad73cb17fa79a625579
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-11-24 16:47:52 +0700

    fix: show ew data

--- src/pages/pages/guard/ew.jsx
+++ src/pages/pages/guard/ew.jsx
@@ -5,9 +5,16 @@ import {Plus} from "lucide-react";
 import {useNavigate} from "react-router-dom";
 import {RoomFilter} from "../../../components/RoomSelect.jsx";
 import {DateRangeFilter} from "../../../components/DateRangeSelect.jsx";
+import {createApiStore} from "../../../util/createApiStore.js";
+import {useViewEffect} from "../../../hooks/useViewEffect.js";
+
+const ewRoomStore = createApiStore("GET", "/ew/room")
 
 export default function GuardEW() {
     const navigate = useNavigate();
+
+    const {data} = useViewEffect(ewRoomStore)
+
     return <LayoutGuard active={"electric-water"}>
         <div className={"flex flex-col gap-3"}>
             <PageHeader title={"Nhập điện nước"}/>
@@ -15,33 +22,44 @@ export default function GuardEW() {
                 <div>
                     <div className={"font-medium mb-3 text-lg"}>Bộ lọc</div>
                     <div className={"flex gap-3 flex-wrap"}>
-                        <RoomFilter />
-                        <DateRangeFilter />
+                        <RoomFilter/>
+                        <DateRangeFilter/>
                     </div>
                 </div>
                 <div className={"ml-auto"}>
-                    <Button onClick={() => navigate("/pages/guard/ew/create")} icon={<Plus size={14}/>}>Tạo bản ghi mới</Button>
+                    <Button onClick={() => navigate("/pages/guard/ew/create")} icon={<Plus size={14}/>}>Tạo bản ghi
+                        mới</Button>
                 </div>
             </div>
             <div className={"section"}>
-                <Table bordered dataSource={[]} columns={[
+                <Table bordered dataSource={data ? data.content : []} columns={[
                     {
-                        title: "Phòng"
+                        title: "Phòng",
+                        dataIndex: ["room", "roomNumber"]
                     },
                     {
-                        title: "Số điện"
+                        title: "Kỳ",
+                        dataIndex: ["semester", "name"]
                     },
                     {
-                        title: "Số nước"
+                        title: "Số điện",
+                        dataIndex: "electric"
                     },
                     {
-                        title: "Sử dụng điện"
+                        title: "Số nước",
+                        dataIndex: "water"
                     },
                     {
-                        title: "Sử dụng nước"
+                        title: "Sử dụng điện",
+                        dataIndex: "electricUsed"
                     },
                     {
-                        title: "Ngày nhập"
+                        title: "Sử dụng nước",
+                        dataIndex: "waterUsed"
+                    },
+                    {
+                        title: "Ngày nhập",
+                        dataIndex: "createDate"
                     },
                 ]}/>
             </div>
